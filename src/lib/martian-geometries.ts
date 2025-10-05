5137a17c8130a95d751b7a5ef0bc54b818d5ae33
Thunder8971 <ricardoparedes0922@gmail.com>
2025-10-05 17:29:35 -0500

    working

--- src/lib/martian-geometries.ts
+++ src/lib/martian-geometries.ts
@@ -1,16 +1,16 @@
 import type {BoundsLiteral} from "./lunar-geometries";
 
 export const GALE_CRATER_BOUNDS: BoundsLiteral = [
-  [-6.4, 136.8],
-  [-4.4, 138.8],
+  [-6.5732988, 135.1842593],
+  [-2.8077809, 139.2413012],
 ];
 
 export const JEZERO_DELTA_BOUNDS: BoundsLiteral = [
-  [18.0, 76.8],
-  [18.9, 78.6],
+  [18.2110274, 77.1604655],
+  [18.7211945, 77.6991776],
 ];
 
 export const OLYMPUS_MONS_BOUNDS: BoundsLiteral = [
-  [16.0, -136.5],
-  [21.0, -132.0],
+  [12, -136],
+  [24, -124],
 ];
