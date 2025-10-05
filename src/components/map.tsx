f49575ffdcb9f3c641712e96e6de46cc2401fa3d
Thunder8971 <ricardoparedes0922@gmail.com>
2025-10-05 09:17:38 -0500

    moon added - flag 2

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,25 +1,85 @@
 "use client";
 
 import {useEffect} from "react";
-import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet";
+import {MapContainer, TileLayer, useMap} from "react-leaflet";
+import type {
+  CRS,
+  LatLngBoundsExpression,
+  LatLngExpression,
+  TileLayerOptions,
+} from "leaflet";
+import {CRS as LeafletCRS, latLngBounds} from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 type MapProps = {
   selectedLayer: string;
 };
 
-const BASE_LAYER = {
+type LayerConfig = {
+  id: string;
+  url: string;
+  attribution: string;
+  tileOptions?: TileLayerOptions;
+  view: {
+    center: LatLngExpression;
+    zoom: number;
+    minZoom?: number;
+    maxZoom?: number;
+  };
+  map?: {
+    crs?: CRS;
+    maxBounds?: LatLngBoundsExpression;
+    maxBoundsViscosity?: number;
+  };
+};
+
+const EARTH_LAYER: LayerConfig = {
+  id: "earth",
   url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  tileOptions: {
+    maxZoom: 19,
+  },
+  view: {
+    center: [51.505, -0.09],
+    zoom: 5,
+    minZoom: 2,
+  },
 };
 
-const LAYERS: Record<string, {url: string; attribution: string}> = {
-  default: BASE_LAYER,
-  earth: BASE_LAYER,
-  moon: BASE_LAYER,
-  mars: BASE_LAYER,
-  stars: BASE_LAYER,
+const MOON_MAX_BOUNDS = latLngBounds([-90, -180], [90, 180]);
+
+const MOON_LAYER: LayerConfig = {
+  id: "moon",
+  url: "https://trek.nasa.gov/tiles/Moon/EQ/LRO_WAC_Mosaic_Global_303ppd_v02/1.0.0/default/default028mm/{z}/{y}/{x}.jpg",
+  attribution:
+    'Imagery courtesy NASA/JPL/USGS <a href="https://trek.nasa.gov/">Trek</a>',
+  tileOptions: {
+    maxZoom: 8,
+    maxNativeZoom: 8,
+    minZoom: 0,
+    noWrap: true,
+  },
+  view: {
+    center: [0, 0],
+    zoom: 2,
+    minZoom: 0,
+    maxZoom: 8,
+  },
+  map: {
+    crs: LeafletCRS.EPSG4326,
+    maxBounds: MOON_MAX_BOUNDS,
+    maxBoundsViscosity: 0.8,
+  },
+};
+
+const LAYERS: Record<string, LayerConfig> = {
+  default: EARTH_LAYER,
+  earth: EARTH_LAYER,
+  moon: MOON_LAYER,
+  mars: EARTH_LAYER,
+  stars: EARTH_LAYER,
 };
 
 function MapResizer() {
@@ -46,22 +106,26 @@ function MapResizer() {
 
 export default function Map({selectedLayer}: MapProps) {
   const layer = LAYERS[selectedLayer] ?? LAYERS.default;
+  const {view, map: mapProps, tileOptions, url, attribution} = layer;
 
   return (
     <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
+      key={layer.id}
+      center={view.center}
+      zoom={view.zoom}
+      minZoom={view.minZoom}
+      maxZoom={view.maxZoom}
       scrollWheelZoom
       className="h-full w-full"
       style={{minHeight: "100%"}}
+      {...mapProps}
     >
       <MapResizer />
-      <TileLayer attribution={layer.attribution} url={layer.url} />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <TileLayer
+        attribution={attribution}
+        url={url}
+        {...tileOptions}
+      />
     </MapContainer>
   );
 }
